1ccc14568ae3d4d34d19cf81d83a8d88bb77ffc2
nyx <loool1912@web.de>
2025-01-26 10:41:58 +0100

    switching to ollama

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Install Ollama https://ollama.com/download
+2. Pull model via e.g. 'ollama run deepseek-r1:8b'
+3. Fill env vars
+4. Run this bot
--- message.py
+++ message.py
@@ -1,51 +1,20 @@
-from asyncio import run
-import asyncio
-
-from langchain.chains.conversation.base import ConversationChain
-from langchain.chains.llm import LLMChain
-from langchain.memory import ConversationBufferMemory
-from langchain_community.llms.gpt4all import GPT4All
-from langchain_core.outputs import LLMResult
-from langchain_core.prompts import PromptTemplate
+from ollama import ChatResponse
+from ollama import chat
 from telegram import Update, Bot
 from telegram.ext import CallbackContext
 
 
-from config import LLM_PATH, ADMINS
-from langchain_core.callbacks import BaseCallbackHandler
-
-
-#memory = ConversationBufferMemory()
-
-
-class TelegramEditCallbackHandler(BaseCallbackHandler):
-    def __init__(self, context, chat_id, message_id):
-        self.context = context
-        self.chat_id = chat_id
-        self.message_id = message_id
-        self.token_buffer = []
-
-    def on_llm_end(self, result: LLMResult, **kwargs) -> None:
-        print(result)
-        asyncio.create_task( self.context.bot.edit_message_text(chat_id=self.chat_id, message_id=self.message_id, text=result.generations[0][0].text))
-
-
-async def start(update: Update, context: CallbackContext):
+async def start(update: Update, _: CallbackContext):
     await update.message.reply_text("Heya! Just chat with me.")
 
 
 async def handle_message(update: Update, context: CallbackContext):
-    msg = await update.message.reply_text("Processing...")
-    # Initialize GPT4All model
-    callback_handler = TelegramEditCallbackHandler(
-        context=context,
-        chat_id=update.effective_chat.id,
-        message_id=msg.message_id
-    )
-    llm = GPT4All(model=LLM_PATH,max_tokens=256,)
-    prompt = PromptTemplate(input_variables=["input_text"], template="You are a helpful assistant for the user. The user is a syrian girl named Ghazal. The user studies linguistics at university. Only provide a single response. Don'T spin the dialog further. Only return your response after <|assistant|>.\nUser: {input_text}")
-    chain = prompt | llm
+    msg = await update.message.reply_text("Thinking...")
 
-    response = chain.invoke({"input_text": update.message.text})
-    print(response)
-    await msg.edit_text(response)
+    response: ChatResponse = chat(model='deepseek-r1:8b', messages=[
+        {
+            'role': 'user',
+            'content': update.message.text,
+        },
+    ])
+    await msg.edit_text(response.message.content)
